ad9bb1be41356656f3a023aba8efda3eaa36bacb
Quarantining <165849060+Quarantining@users.noreply.github.com>
2024-08-24 21:43:43 -0700

    Update number-guessing-game.py
    Added versions variable to display current version on title screen. Added function that contains all game code. Added title screen that displays 5 game options (easy, normal, hard, impossible, custom). Added If/else to select which mode. Added option to quit game from title screen. Added simple try/except valueerror for initial user selection. Also added some comments throughout the code.

--- number-guessing-game/number-guessing-game.py
+++ number-guessing-game/number-guessing-game.py
@@ -1,17 +1,81 @@
-#1.0
 import numpy as np
+v = "1.1"
 
-random_number = np.random.randint(0,100)
-print("Guess a random number between 1-100")
+# function for game code
+def number_guessing_game(difficulty):
 
-while True:
-    user_input = int(input("> "))
-    if user_input == random_number:
-        print(f'Correct, the number was {random_number}')
-        break
-    elif user_input > random_number:
-        print("Too high")
-    elif user_input < random_number:
-        print("Too low")
+    # random number (selected by user)
+    random_number = difficulty
+
+    # while loop runs until random_number is guessed by user.
+    while True:
+        # user input (only takes ints).
+        user_input = int(input("> "))
+
+        # if user guesses correctly
+        if user_input == random_number:  #
+            print(f'Correct, the number was {random_number}')
+            break  # ends program.
+
+        # if user guess is too high.
+        elif user_input > random_number:
+            print("Too high!")
+        # if user guess is too low.
+        elif user_input < random_number:
+            print("Too low!")
+        # for anything else.
+        else:
+            print("Error")
+
+# title screen
+print(f"""Welcome to 'Number Guessing game'!
+
+Please select difficulty:
+
+easy --> 1
+normal --> 2
+hard --> 3
+impossible --> 4  
+custom --> 5   
+
+version {v}
+""")
+
+# user selection mode
+try:
+    user_mode_selection = int(input("> "))
+
+    # selects "easy" mode.
+    if user_mode_selection == 1: # selects the easy mode by the user.
+        random_number = np.random.randint(0,10) # sets the random number
+        print("Guess the random number between 0-10") # tells user (this is hard coded for each mode).
+        number_guessing_game(random_number) # inputs the random number into the function (that is then used for the game).
+    # selects "normal" mode.
+    elif user_mode_selection == 2:
+        random_number = np.random.randint(0,50)
+        print("Guess the random number between 0-50")
+        number_guessing_game(random_number)
+    # selects "hard" mode.
+    elif user_mode_selection == 3:
+        random_number = np.random.randint(0,100)
+        print("Guess the random number between 0-100")
+        number_guessing_game(random_number)
+    # selects "impossible" mode.
+    elif user_mode_selection == 4:
+        random_number = np.random.randint(0,1000)
+        print("Guess the random number between 0-1000")
+        number_guessing_game(random_number)
+    # select "custom" mode.
+    elif user_mode_selection == 5:
+        user_number_selection = int(input("Select custom number: "))
+        random_number = np.random.randint(0,user_number_selection)
+        print(f"Guess the random number between 0-{user_mode_selection}")
+        number_guessing_game(random_number)
+    # ends program.
+    elif user_mode_selection == 6:
+        print("Stopping program..")
+    # for anything else.
     else:
         print("error")
+except ValueError:
+    print("Value Error.")
