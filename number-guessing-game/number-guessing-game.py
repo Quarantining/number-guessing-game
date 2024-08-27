49af31cfad3bb779eedce91648db73a2afc07458
K. R. <165849060+Quarantining@users.noreply.github.com>
2024-08-26 17:26:38 -0700

    v1.2.4 Update number-guessing-game.py
    Fixed AI total guess stats (now accumulates each game instead of being replaced by the last games total guesses). Added a quit option for all modes, and quit option to close program. Added comments to code.

--- number-guessing-game/number-guessing-game.py
+++ number-guessing-game/number-guessing-game.py
@@ -1,5 +1,5 @@
 import numpy as np
-v = "1.2.3" # version (displays on title screen).
+v = "1.2.4" # version (displays on title screen).
 
 # Global Stats
 wins = 0
@@ -30,27 +30,39 @@ def number_guessing_game(difficulty):
     f_wins = 0 # when user selects correct number
     f_too_high = 0 # when user is too high
     f_too_low = 0 # when user is too low
-    f_user_guesses = 0
+    f_user_guesses = 0 # total guesses (var not used).
 
     # while loop runs until random_number is guessed by user.
     while True:
         # user input (only takes ints).
-        user_input = int(input("> "))
-        f_user_guesses += 1
+        user_input = int(input("> ")) # 0 to end program, > 0 to guess number.
+        if user_input == 0: # stops program
+            confirm_stop = input("do you want to quit game? (yes/no): ") # confirms that user wants to quit
+            if confirm_stop == "yes": # ends game
+                print("game quit.")
+                return f_wins, f_too_high, f_too_low, f_user_guesses # returns f stats and game ends
+            elif confirm_stop == "no": # returns user game
+                print("game not quit.")
+            else: # if user doesn't type "yes" or "no"
+                print("Error. returning to game.") # returns user to game.
+
         # if user guesses correctly
-        if user_input == random_number:  #
+        elif user_input == random_number:  #
             print(f'You win! The correct number was {random_number}!')
-            f_wins += 1 # adds +1 to f_wins when user guesses correct number.
+            f_wins += 1 # adds +1 to f_wins when user guesses correct number.\
+            f_user_guesses += 1 # adds guess
             return f_wins, f_too_high, f_too_low, f_user_guesses # packs f stats (gets added to global stats outside of function).
 
         # if user guess is too high.
         elif user_input > random_number:
             print("Too high!")
+            f_user_guesses += 1
             f_too_high += 1 # adds 1 to too_high counter
 
         # if user guess is too low.
         elif user_input < random_number:
             print("Too low!")
+            f_user_guesses += 1
             f_too_low += 1 # adds 1 to too_low counter
 
         # for anything else.
@@ -59,7 +71,7 @@ def number_guessing_game(difficulty):
 
 # function for AI model to guess instead of user.
 def ai_player_mode(difficulty):
-    random_number = np.random.randint(0,1000) # var that sets random num.
+    random_number = np.random.randint(1,1000) # var that sets random num.
     guess_count = 0 # times the AI guessed.
 
     f_ai_wins = 0  # when AI selects correct number
@@ -68,7 +80,7 @@ def ai_player_mode(difficulty):
     f_ai_guesses = guess_count
 
     for i in range(99999): # the AI gets 99999 attempts to guess the correct num.
-        ai_guess = np.random.randint(0,1000) # ai guess is randomly generated.
+        ai_guess = np.random.randint(1,1000) # ai guess is randomly generated.
         guess_count += 1 # adds +1 guess each iteration of for loop.
 
         if ai_guess == random_number:
@@ -105,7 +117,7 @@ while True: # while loop so game doesn't end after game is finished.
 
         # selects "easy" mode.
         if user_mode_selection == 1: # selects the easy mode by the user.
-            random_number = np.random.randint(0,10) # sets the random number
+            random_number = np.random.randint(1,10) # sets the random number
             print("Guess the random number between 0-10") # tells user (this is hard coded for each mode).
             f_wins, f_too_high, f_too_low, f_user_guesses = number_guessing_game(random_number) # inputs the random number into the function (that is then used for the game).
                                                                                 # f_wins, f_too_high, etc stand for function wins, fucntion too_highs, etc. It takes the values from that game and sets these f to values.
@@ -117,7 +129,7 @@ while True: # while loop so game doesn't end after game is finished.
 
         # selects "normal" mode.
         elif user_mode_selection == 2:
-            random_number = np.random.randint(0,50)
+            random_number = np.random.randint(1,50)
             print("Guess the random number between 0-50")
             f_wins, f_too_high, f_too_low, f_user_guesses = number_guessing_game(random_number)
             wins += f_wins
@@ -128,7 +140,7 @@ while True: # while loop so game doesn't end after game is finished.
 
         # selects "hard" mode.
         elif user_mode_selection == 3:
-            random_number = np.random.randint(0,100)
+            random_number = np.random.randint(1,100)
             print("Guess the random number between 0-100")
             f_wins, f_too_high, f_too_low, f_user_guesses = number_guessing_game(random_number)
             wins += f_wins
@@ -139,7 +151,7 @@ while True: # while loop so game doesn't end after game is finished.
 
         # selects "impossible" mode.
         elif user_mode_selection == 4:
-            random_number = np.random.randint(0,1000000)
+            random_number = np.random.randint(1,1000000)
             print("Guess the random number between 0-1000000")
             f_wins, f_too_high, f_too_low, f_user_guesses = number_guessing_game(random_number)
             wins += f_wins
@@ -151,7 +163,7 @@ while True: # while loop so game doesn't end after game is finished.
         # select "custom" mode.
         elif user_mode_selection == 5:
             user_number_selection = int(input("Select custom number: "))
-            random_number = np.random.randint(0,user_number_selection)
+            random_number = np.random.randint(1,user_number_selection)
             print(f"Guess the random number between 0-{user_number_selection}")
             f_wins, f_too_high, f_too_low, f_user_guesses = number_guessing_game(random_number)
             wins += f_wins
@@ -166,7 +178,7 @@ while True: # while loop so game doesn't end after game is finished.
             ai_wins += f_ai_wins
             ai_too_high += f_ai_too_high
             ai_too_low += f_ai_too_low
-            ai_guesses = f_ai_too_low + f_ai_too_high
+            ai_guesses += f_ai_too_low + f_ai_too_high
             print(title_screen)
 
 
@@ -192,9 +204,15 @@ AI Stats:
 """) # these are the global stats (not f stats)
 
         # ends program.
-        elif user_mode_selection == 7:
-            print("Stopping program..")
-            break # ends program
+        elif user_mode_selection == 0:
+            confirm_stop = input("Do you want to quit program? Your stats wont be saved. (yes/no): ") # checks with user to confirm quiting
+            if confirm_stop == "yes": # if user types "yes"
+                print("program stopped.") # tells user program is stopping
+                break # ends program
+            elif confirm_stop == "no": # if user types "no"
+                print(title_screen) # program does not stop
+            else: # for any other input
+                print("Error") # prints an error message
 
         # for anything else.
         else:
